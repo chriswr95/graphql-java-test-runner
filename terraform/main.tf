8404ad01e22355d32cc823ea2d5b0a73e6fea05c
Adarsh Jaiswal <adajaiswal@twitter.com>
2022-07-21 11:30:21 -0400

    Added cloud manager api resource and firestore. Tested on a brand new gcp project. It's ok to get error for firestore if it's already enabled once.

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,16 +6,16 @@ terraform {
   }
 }
 
+locals {
+  cred = file("cred.json")
+  email = lookup(jsondecode(local.cred), "client_email")
+}
+
 provider "google" {
   credentials = local.cred
   project = var.project_id
   region  = var.region
   zone    = "us-central1-c"
-}
-
-locals {
-  cred = file("cred.json")
-  email = lookup(jsondecode(local.cred), "client_email")
 }
 
 # Define and deploy a workflow
@@ -48,13 +48,17 @@ resource "google_cloud_tasks_queue" "test_runner_tasks_queue" {
     max_doublings = 1
   }
 
+  depends_on = [ google_project_service.cloud_tasks_api]
 }
 
-# Firestore
-# resource "google_app_engine_application" "firestore" {
-#   project = var.project_id
-#   location_id = "us-central"
-#   database_type = "CLOUD_FIRESTORE"
+# Enable Firestore, this operation will be successful when initializing
+# the project for the first time. Firestore once enabled can never be disabled on the same project.
+# If terraform appy is called multiple times for the same project it's ok to get the below error for firestore.
+# Error 409: This application already exists and cannot be re-created.
+resource "google_app_engine_application" "firestore" {
+  project = var.project_id
+  location_id = "us-central"
+  database_type = "CLOUD_FIRESTORE"
 
-#   depends_on = [google_project_service.app_engine_api]
-# }
+  depends_on = [google_project_service.app_engine_api]
+}
--- terraform/projects.tf
+++ terraform/projects.tf
@@ -1,23 +1,32 @@
+# Enable cloud resource manager API, required to enable dependent API's.
+resource "google_project_service" "cloud_manager_api" {
+  service   = "cloudresourcemanager.googleapis.com"
+}
+
 # Enable Workflows API
 resource "google_project_service" "workflows" {
   service = "workflows.googleapis.com"
   disable_dependent_services = true
+  depends_on = [google_project_service.cloud_manager_api]
 }
 
 # Enable AppEngine API - required to create datastore
 resource "google_project_service" "app_engine_api" {
   service = "appengine.googleapis.com"
   disable_dependent_services = true
+  depends_on = [google_project_service.cloud_manager_api]
 }
 
 # Enable ComputeEngine API - required to create compute engine instance
 resource "google_project_service" "compute_engine_api" {
   service = "compute.googleapis.com"
   disable_dependent_services = true
+  depends_on = [google_project_service.cloud_manager_api]
 }
 
 # Enable Cloud Tasks Queue API - required to create a queue
 resource "google_project_service" "cloud_tasks_api" {
   service = "cloudtasks.googleapis.com"
   disable_dependent_services = true
+  depends_on = [google_project_service.cloud_manager_api]
 }
